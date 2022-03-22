3c73deb5b69dea46aa4782cc784aec2afa682a2a
thereseml <therese.lindholm@medieinstitutet.se>
2022-03-22 08:26:04 +0100

    get animals from api

--- src/components/GetAnimals.tsx
+++ src/components/GetAnimals.tsx
@@ -1,13 +1,22 @@
 import axios from "axios"
+import { useEffect } from "react";
 import { Animals } from "../models/Animals";
 
 export const GetAnimals = () => {
 
-    axios.get<Animals>('https://animals.azurewebsites.net/api/animals')
-    .then(response => {
-        console.log(response.data);
-        
-    })
 
-    return (<p>Hejhej</p>)
+    useEffect(()=> {
+        getData()
+    }, []);
+
+    function getData(){
+        axios.get<Animals[]>('https://animals.azurewebsites.net/api/animals')
+        .then(response => { 
+            let AnimalsFromApi = response.data.map((animal: Animals) => {
+                return 
+            })
+        })
+    }
+
+    return (<ul>{Animals.name}</ul>)
 }
